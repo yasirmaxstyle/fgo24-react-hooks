a5bb508df5eff1de2cfaab2ddcc8074f63d725e3
yasirmaxstyle <yasirmu77@gmail.com>
2025-05-16 02:00:27 +0700

    feat: add fetch program using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chars, setChar] = useState([])
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then(({ data }) => {
+        setChar(data.results)
+      })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-wrap gap-3 justify-center'>
+      {chars.map((char, idx) =>
+        <div key={idx} className='text-center flex flex-col gap-3 p-3 shadow-md rounded'>
+          <img src={char.image} alt={char.name} />
+          <div className='text-xl font-semi-bold'>{char.name}</div>
+        </div>
+      )}
+    </div>
   )
 }
 
